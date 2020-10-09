1df723693ed6036dbd5a0cb22289924898865acc
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-10-08 21:20:56 -0400

    complete moving zeros algo

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,23 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
 
-    pass
+    zero_counter = 0
+
+    for i in range(len(arr) - 1):
+        if arr[i] == 0:
+            zero_counter += 1
+    #loop through arr,
+    # if i equals 0, then swap with the right
+    while zero_counter != 0:
+        for i in range(len(arr) - 1):
+            if arr[i] == 0:
+                print(arr)
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        zero_counter -= 1
+
+
+    return arr
 
 
 if __name__ == '__main__':
